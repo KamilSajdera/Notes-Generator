bb5c42ad9a8e36fb278b651d91181bdc19029ee2
Kamil Sajdera <kamilsajdera@gmail.com>
2023-06-05 11:59:09 +0200

    Fix displaying pauses if pitch was semitone

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,26 +4,11 @@ import './Note.css';
 const Note = props => 
 {
 
-    const noteDetailsHandler = () => {
-        console.log(props.notes)
-        console.log(props.id)
-
-         // Znajdź obiekt w tablicy props.notes o pasującym props.id
-         const note = props.notes.find((note) => note.id === props.id);
-
-         if (note) {
-             console.log('Id: ', note.id)
-             console.log('Value:', note.value);
-             console.log('Latency:', note.latency);
-             console.log('Pitch:', note.pitch);
-         }
-    }
-
-    if(props.type === 'sound')
-        return <li className={'note ' + props.className} onClick={noteDetailsHandler}>{props.children}</li>
+    if(props.type === 'rest')
+        return <li className={'rest r' + props.className.substring(0, 3).trim()}>{props.children}</li>
   
     else 
-        return <li className={'rest r' + props.className}>{props.children}</li>
+        return <li className={'note ' + props.className}>{props.children}</li>
       
 }
         
