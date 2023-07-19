bfef42a73272b63a0fb27b5bf9e229d4e4dce378
Kamil Sajdera <kamilsajdera@gmail.com>
2023-07-19 20:56:37 +0200

    New design of pitch & value options

--- src/components/PitchOptions.js
+++ src/components/PitchOptions.js
@@ -1,46 +1,101 @@
-import React from "react";
-import './SettingsWrapper.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./SettingsWrapper.css";
 
-const PitchOptions = props => {
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-    const pitchItemsHandler = event => props.onPitchOptions(`pitch-${event.target.value}`)
+library.add(faCaretDown);
 
-    return (
-        <div className='property-item'>
-                <h3>Choose a pitch</h3>
-                 
-                <select id="pitch-items" onChange={pitchItemsHandler} disabled={ props.disabled ? true : false}>
-                    <option value="none">Select</option>
-                    <option value="C">C</option>
-                    <option value="Cis">C#</option>
-                    <option value="D">D</option>
-                    <option value="Es">Es</option>
-                    <option value="E">E</option>
-                    <option value="F">F</option>
-                    <option value="Fis">F#</option>
-                    <option value="G">G</option>
-                    <option value="Gis">G#</option>
-                    <option value="A">A</option>
-                    <option value="B">b</option>
-                    <option value="H">H</option>
-                    <option value="C2">C2</option>
-                    <option value="Cis2">C#2</option>
-                    <option value="D2">D2</option>
-                    <option value="Es2">Es2</option>
-                    <option value="E2">E2</option>
-                    <option value="F2">F2</option>
-                    <option value="Fis2">F#2</option>
-                    <option value="G2">G2</option>
-                    <option value="Gis2">G#2</option>
-                    <option value="A2">A2</option>
-                    <option value="B2">b2</option>
-                    <option value="H2">H2</option>
-                    <option value="C3">C3</option>
-                    <option value="Cis3">C#3</option>
+const PitchOptions = ({ onPitchOptions }) => {
+  const listOfItemsClickRef = useRef(null);
+  const listOfItemsRef = useRef(null);
+  const [pitch, setPitch] = useState("none");
 
-                </select>
-            </div>
-    )
-}
+  useEffect(() => {
+    const listOfItemsClick = listOfItemsClickRef.current;
+    const listOfItems = listOfItemsRef.current;
+
+    const showListOfItems = () => {
+      listOfItems.classList.add("show_list");
+    };
+
+    const selectOptionHandler = (e) => {
+      listOfItems.classList.remove("show_list");
+      listOfItemsClick.innerHTML = e.target.innerText;
+      setPitch(`pitch-${e.target.innerText}`);
+    };
+
+    listOfItemsClick.addEventListener("click", showListOfItems);
+
+    const listItemElements = listOfItems.querySelectorAll(
+      ".option-container__listItem"
+    );
+
+    listItemElements.forEach((item) => {
+      item.addEventListener("click", selectOptionHandler);
+    });
+
+    listOfItems.addEventListener("mouseleave", () => {
+      listOfItems.classList.remove("show_list");
+    });
+
+    return () => {
+      listOfItemsClick.removeEventListener("click", showListOfItems);
+      listItemElements.forEach((item) => {
+        item.removeEventListener("click", selectOptionHandler);
+      });
+    };
+  }, []); /// ignore missing dependencies
+
+  useEffect(() => {
+    onPitchOptions(pitch);
+  }, [pitch]);
+
+  return (
+    <div className="property-item">
+      <h3>Choose a pitch</h3>
+
+      <div className="option-container">
+        <div
+          className="option-container__enteredOption"
+          ref={listOfItemsClickRef}
+        >
+          Select
+        </div>
+        <div className="option-container__arrow">
+          <FontAwesomeIcon icon={faCaretDown} />
+        </div>
+        <ul className="option-container__list" ref={listOfItemsRef}>
+          <li className="option-container__listItem">C</li>
+          <li className="option-container__listItem">Cis</li>
+          <li className="option-container__listItem">D</li>
+          <li className="option-container__listItem">Es</li>
+          <li className="option-container__listItem">E</li>
+          <li className="option-container__listItem">F</li>
+          <li className="option-container__listItem">Fis</li>
+          <li className="option-container__listItem">G</li>
+          <li className="option-container__listItem">Gis</li>
+          <li className="option-container__listItem">A</li>
+          <li className="option-container__listItem">b</li>
+          <li className="option-container__listItem">H</li>
+          <li className="option-container__listItem">C2</li>
+          <li className="option-container__listItem">Cis2</li>
+          <li className="option-container__listItem">D2</li>
+          <li className="option-container__listItem">Es2</li>
+          <li className="option-container__listItem">E2</li>
+          <li className="option-container__listItem">F2</li>
+          <li className="option-container__listItem">Fis2</li>
+          <li className="option-container__listItem">G2</li>
+          <li className="option-container__listItem">Gis2</li>
+          <li className="option-container__listItem">A2</li>
+          <li className="option-container__listItem">b2</li>
+          <li className="option-container__listItem">H2</li>
+          <li className="option-container__listItem">C3</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default PitchOptions;
