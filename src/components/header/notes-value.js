f2ac2077176c039159008e3f70d0b55a90f650ff
Kamil Sajdera <kamilsajdera@gmail.com>
2024-11-24 20:22:28 +0100

    Replaced divs into inputs type = radio

--- src/components/header/notes-value.js
+++ src/components/header/notes-value.js
@@ -1,19 +1,62 @@
+import { useState } from "react";
+
 export default function NotesValue() {
+  const [valueArray, setValueArray] = useState([
+    {
+      id: 1,
+      name: "Semibreve",
+      division: "4/4",
+      value: "n16",
+      disabled: false,
+    },
+    {
+      id: 2,
+      name: "Minim",
+      division: "2/4",
+      value: "n8",
+      disabled: false,
+    },
+    {
+      id: 3,
+      name: "Crotchet",
+      division: "1/4",
+      value: "n4",
+      disabled: false,
+    },
+    {
+      id: 4,
+      name: "Quaver",
+      division: "1/8",
+      value: "n2",
+      disabled: false,
+    },
+  ]);
+  
   return (
-    <div className="toolbar-row" style={{ marginTop: "10px", width: "100%", columnGap: "20px" }} >
+    <div
+      className="toolbar-row"
+      style={{ marginTop: "10px", width: "100%", columnGap: "20px" }}
+    >
       <h3>Value</h3>
-      <div className="note-value">
-        Semibreve <span className="note-division">(4/4)</span>
-      </div>
-      <div className="note-value">
-        Minim <span className="note-division">(2/4)</span>
-      </div>
-      <div className="note-value">
-        Crotchet <span className="note-division">(1/4)</span>
-      </div>
-      <div className="note-value">
-        Quaver <span className="note-division">(1/8)</span>
-      </div>
+      {valueArray.map((value) => (
+        <div
+          className={`note-value ${value.disabled ? "disabled" : ""}`}
+          key={value.id}
+        >
+          <input
+            type="radio"
+            name="value"
+            className="value-radio"
+            value={value.value}
+            id={value.name}
+            disabled={value.disabled}
+          />
+          <label htmlFor={value.name} className="value-label">
+            {value.name}{" "}
+            <span className="note-division">({value.division})</span>
+          </label>
+        </div>
+      ))}
     </div>
   );
 }
