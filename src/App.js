5e8a5704ff3d5c52e674e7faf6b8f6a0e6f02750
Kamil Sajdera <kamilsajdera@gmail.com>
2024-11-29 12:29:39 +0100

    Added <Header /> as <MetronomeProvider /> child

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
 import React, { useCallback, useEffect, useState } from "react";
 
 import NotesWrapper from "./components/NotesWrapper";
-import SettingsWrapper from './components/SettingsWrapper';
+import SettingsWrapper from "./components/SettingsWrapper";
 import GenericSettings from "./components/GenericSettings";
 
 import NotesContext from "./store/notes-context";
 import { MetronomeProvider } from "./store/metronome-context";
 
-import './App.css';
+import "./App.css";
 import Header from "./components/header/header";
 
 function App() {
-
   const [arrayNotes, setArrayNotes] = useState([]);
   const [noteKey, setNoteKey] = useState();
   const [currentLatency, setCurrentLatency] = useState();
   const [isAvailable, setIsAvailable] = useState(true);
 
-  const addNoteHandler = enteredNote => {
+  const addNoteHandler = (enteredNote) => {
     setArrayNotes((prevNote) => {
-      return [...prevNote, enteredNote]
-     })
-  }
+      return [...prevNote, enteredNote];
+    });
+  };
 
-  const setKeyHandler = useCallback((key) => {setNoteKey(key)}, []) 
-  const currentLatencyHandler = value => setCurrentLatency(value);
+  const setKeyHandler = useCallback((key) => {
+    setNoteKey(key);
+  }, []);
+  const currentLatencyHandler = (value) => setCurrentLatency(value);
 
   const removeNoteHandler = () => {
     setArrayNotes((prevNotes) => {
       const updatedNotes = [...prevNotes];
       updatedNotes.pop();
       return updatedNotes;
-    })
-  }
-
+    });
+  };
 
   useEffect(() => {
     function handleResize() {
-      if(window.innerWidth < 879) 
-        setIsAvailable(false)
-      else
-        setIsAvailable(true)
+      if (window.innerWidth < 879) setIsAvailable(false);
+      else setIsAvailable(true);
     }
 
     handleResize();
@@ -52,28 +50,32 @@ function App() {
   }, []);
 
   return (
-    <NotesContext.Provider value={{
-      notes: arrayNotes, 
-      notesKey: noteKey, 
-      currentLatency: currentLatency,
-      onRemoveNote: removeNoteHandler
-      }}>
-      <div className='container'>
-          <Header />
-          { isAvailable ? 
-            <MetronomeProvider>
-            <GenericSettings onSetKey={setKeyHandler}/>
-            <div className='generator-elements'>
+    <NotesContext.Provider
+      value={{
+        notes: arrayNotes,
+        notesKey: noteKey,
+        currentLatency: currentLatency,
+        onRemoveNote: removeNoteHandler,
+      }}
+    >
+      <div className="container">
+        {isAvailable ? (
+          <MetronomeProvider>
+            <Header />
+            <GenericSettings onSetKey={setKeyHandler} />
+            <div className="generator-elements">
               <NotesWrapper onCurrentLatency={currentLatencyHandler} />
-              <SettingsWrapper onAddNote={addNoteHandler} />     
-            </div>  
-          </MetronomeProvider> : 
-          <p style={{paddingTop: 70, marginInline: 'auto'}}>You cannot use this app in this resolution :(</p>
-          }
-          
+              <SettingsWrapper onAddNote={addNoteHandler} />
+            </div>
+          </MetronomeProvider>
+        ) : (
+          <p style={{ paddingTop: 70, marginInline: "auto" }}>
+            You cannot use this app in this resolution :(
+          </p>
+        )}
       </div>
     </NotesContext.Provider>
-  )
+  );
 }
 
 export default App;
